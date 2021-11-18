60702f349c3753f569e7019fc55f0ef0cac54001
WWinters <WWinters@jobrien.com>
2021-11-18 17:00:54 -0500

    Initial commit.  reCAPTCHA checkbox renders

---
+++ Workspace/recaptcha/Modules/recaptcha/JavaScript/recaptcha.View.js
@@ -0,0 +1,59 @@
+// @module JOB.recaptcha.recaptcha
+define('JOB.recaptcha.recaptcha.View'
+,	[
+	'job_recaptcha_recaptcha.tpl'
+	
+	,	'JOB.recaptcha.recaptcha.SS2Model'
+	
+	,	'Backbone'
+    ]
+, function (
+	job_recaptcha_recaptcha_tpl
+	
+	,	recaptchaSS2Model
+	
+	,	Backbone
+)
+{
+    'use strict';
+
+	// @class JOB.recaptcha.recaptcha.View @extends Backbone.View
+	return Backbone.View.extend({
+
+		template: job_recaptcha_recaptcha_tpl
+
+	,	initialize: function (options) {
+
+			/*  Uncomment to test backend communication with an example service
+				(you'll need to deploy and activate the extension first)
+			*/
+
+			// this.model = new recaptchaModel();
+			// var self = this;
+         	// this.model.fetch().done(function(result) {
+			// 	self.message = result.message;
+			// 	self.render();
+      		// });
+		}
+
+	,	events: {
+		}
+
+	,	bindings: {
+		}
+
+	, 	childViews: {
+
+		}
+
+		//@method getContext @return JOB.recaptcha.recaptcha.View.Context
+	,	getContext: function getContext()
+		{
+			//@class JOB.recaptcha.recaptcha.View.Context
+			this.message = this.message || 'Hello World!!'
+			return {
+				message: this.message
+			};
+		}
+	});
+});
